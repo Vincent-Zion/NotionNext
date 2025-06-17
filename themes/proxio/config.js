57c5f3252726e334626e752f9e2bf389a3aa4279
Vincent-Zion <liuwei.zion@zohomail.com>
2025-06-17 23:38:46 +0800

    Update config.js
    自定义主题

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -75,6 +75,7 @@ const CONFIG = {
         { title: 'Freelance Product Designer', bio: '2022-Now', text: 'Now I design user-centric products that align with client visions. As a freelance product designer, I collaborate with startups and established companies, crafting solutions that elevate user experiences and increase engagement across both digital and physical interfaces.' }
     ],
 
+    /*
     // 首页用户测评区块
     PROXIO_TESTIMONIALS_ENABLE: true, // 测评区块开关
     PROXIO_TESTIMONIALS_TITLE: '用户反馈',
@@ -86,7 +87,7 @@ const CONFIG = {
     PROXIO_TESTIMONIALS_BUTTON_URL: '/about',
     PROXIO_TESTIMONIALS_BUTTON_TEXT: '联系我',
 
-    /*// 这里不支持CONFIG和环境变量，需要一一修改此处代码。
+    // 这里不支持CONFIG和环境变量，需要一一修改此处代码。
     PROXIO_TESTIMONIALS_ITEMS: [
         {
             PROXIO_TESTIMONIALS_ITEM_TEXT:
@@ -144,18 +145,37 @@ const CONFIG = {
     ],
     */
 
-    //   FAQ 常见问题模块
-    PROXIO_FAQ_ENABLE: true, // 常见问题模块开关
-    PROXIO_FAQ_TITLE: '常见问题解答',
-    PROXIO_FAQ_TEXT_1: '有任何问题吗？请看这里',
-    PROXIO_FAQ_TEXT_2: '我们收集了常见的用户疑问',
-    PROXIO_FAQS: [
-        { q: 'NotionNext有帮助文档吗？', a: 'NotionNext提供了<a href="https://docs.tangly1024.com/about" className="underline">帮助文档</a>，操作<a href="https://www.bilibili.com/video/BV1fM4y1L7Qi/" className="underline">演示视频</a>，以及<a href="https://docs.tangly1024.com/article/chat-community" className="underline">交流社群</a>来协助您完成网站的搭建部署' },
-        { q: '部署后要如何编写文章？', a: '您可以在Notion中之间添加或修改类型为Post的页面，内容将被实时同步在站点中，详情参考<a className="underline" href="https://docs.tangly1024.com/article/start-to-write">《帮助文档》</a>' },
-        { q: '站点部署失败，更新失败？', a: '通常是配置修改错误导致，请检查配置或者重试操作步骤，或者通过Vercel后台的Deployments中找到错误日志，并向网友求助' },
-        { q: '文章没有实时同步？', a: '先检查Notion_Page_ID是否正确配置，其次由于博客的每个页面都有独立缓存，刷新网页后即可解决' },
-    ],
 
+    // FAQ 常见问题模块
+    PROXIO_FAQ_ENABLE: true,
+    PROXIO_FAQ_TITLE: '关于我',
+    PROXIO_FAQ_TEXT_1: '想了解我多一点？请往下看',
+    PROXIO_FAQ_TEXT_2: '我用简洁的问答方式，写下我的真实与热爱',
+    PROXIO_FAQS: [
+    {
+        q: '你相信什么？',
+        a: '我相信，生命不只是呼吸，而是被爱激活，被话语引导，被永恒吸引。'
+    }  
+    {
+        q: '你是做什么的？',
+        a: '我是一个喜欢研究圣经、探寻真理的人，平时也写一点嵌入式代码，但我的重心更在内在生命的成长。'
+    },
+    {
+        q: '你平时都在做些什么？',
+        a: '我每周参加5次线上学习，分享圣经感悟，也在小组中陪伴成长，思考如何将爱活出来。'
+    },
+    {
+        q: '你为什么创建这个网站？',
+        a: '我希望这里成为一块净土，用来记录我信仰的旅程、生命的见证，也分享一点技术笔记，方便有需要的人。'
+    },
+    {
+        q: '你对生活有什么看法？',
+        a: '人生就像旷野行走，有时候风沙扑面，但只要眼光不离开那道光，终会找到方向与平安。'
+    },
+    ]
+
+
+    /*
     // 关于作者区块
     PROXIO_ABOUT_ENABLE: false, // 关于作者区块区块开关
     PROXIO_ABOUT_TITLE: '关于作者',
@@ -174,6 +194,7 @@ const CONFIG = {
 
     PROXIO_ABOUT_BUTTON_URL: '/about',
     PROXIO_ABOUT_BUTTON_TEXT: '关于我',
+    */
 
     // 横向滚动文字
     PROXIO_BRANDS_ENABLE: true, // 滚动文字
