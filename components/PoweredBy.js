8216df2a131948085e36fea8feb2abd3a635b41a
Vincent-Zion <liuwei.zion@zohomail.com>
2025-06-17 20:03:58 +0800

    Update PoweredBy.js
    修改页脚，不显示原作者信息

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -5,9 +5,10 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 
-/*
+
 export default function PoweredBy(props) {
   return (
+    /*
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
@@ -17,6 +18,7 @@ export default function PoweredBy(props) {
       </a>
       .
     </div>
+    */
   )
 }
-*/
+
