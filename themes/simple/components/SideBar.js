ebfc2bc4e1b430c2df3d13d6ceee111455209222
Vincent-Zion <liuwei.zion@zohomail.com>
2025-07-07 22:55:33 +0800

    Update SideBar.js
    恢复

--- themes/simple/components/SideBar.js
+++ themes/simple/components/SideBar.js
@@ -1,5 +1,5 @@
 import { AdSlot } from '@/components/GoogleAdsense'
-//import Live2D from '@/components/Live2D'
+import Live2D from '@/components/Live2D'
 import Announcement from './Announcement'
 import Catalog from './Catalog'
 import WWAds from '@/components/WWAds'
