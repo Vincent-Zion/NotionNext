f9cf727d9296dd771c5d28c2e78d78a412cb042a
Vincent-Zion <215732808+Vincent-Zion@users.noreply.github.com>
2026-01-08 00:07:38 +0800

    Update SideBar.js
    修改（删掉）侧边栏的图标

--- themes/heo/components/SideBar.js
+++ themes/heo/components/SideBar.js
@@ -18,13 +18,13 @@ const SideBar = (props) => {
         <div id='side-bar'>
             <div className="h-52 w-full flex justify-center">
                 <div>
-                    <div onClick={() => { router.push('/') }}
-                        className='justify-center items-center flex hover:rotate-45 py-6 hover:scale-105 dark:text-gray-100  transform duration-200 cursor-pointer'>
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <LazyImage src={siteInfo?.icon} className='rounded-full' width={80} alt={siteConfig('AUTHOR')} />
-                    </div>
-                    <MenuGroupCard {...props} />
-                </div>
+//                    <div onClick={() => { router.push('/') }}
+//                        className='justify-center items-center flex hover:rotate-45 py-6 hover:scale-105 dark:text-gray-100  transform duration-200 cursor-pointer'>
+//                        {/* eslint-disable-next-line @next/next/no-img-element */}
+//                       <LazyImage src={siteInfo?.icon} className='rounded-full' width={80} alt={siteConfig('AUTHOR')} />
+//                   </div>
+//                  <MenuGroupCard {...props} />
+                </div>        
             </div>
             <MenuListSide {...props} />
         </div>
