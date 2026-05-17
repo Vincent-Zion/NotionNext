6220fe49bebb08067e7c8ecdc64efebfe0e508d9
Vincent-Zion <215732808+Vincent-Zion@users.noreply.github.com>
2026-05-17 12:22:34 +0800

    Update blog name in typography config

--- themes/typography/config.js
+++ themes/typography/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // 博客標題 雙語言
-  TYPOGRAPHY_BLOG_NAME: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || '在灵知彼岸遇见真理',
+  TYPOGRAPHY_BLOG_NAME: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || '你当刚强壮胆',
   TYPOGRAPHY_BLOG_NAME_EN: process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME_EN || process.env.NEXT_PUBLIC_TYPOGRAPHY_BLOG_NAME || 'Beyond Spiritual Knowledge',
 
   TYPOGRAPHY_POST_AD_ENABLE: process.env.NEXT_PUBLIC_TYPOGRAPHY_POST_AD_ENABLE || false, // 文章列表是否插入广告
